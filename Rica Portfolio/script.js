94ff96e51237d3098159dcfaebeceeef6a88b445
Jideeh1 <keonagjd@gmail.com>
2025-01-11 02:28:58 +0800

    Add files via upload

--- Rica Portfolio/script.js
+++ Rica Portfolio/script.js
@@ -1,3 +1,12 @@
     setTimeout(() => {
         document.getElementById('spsc').classList.toggle('fade');
     }, 2000);
+
+    function confirmEmail() {
+        if (confirm("Do you want to proceed to email?")) {
+            window.location.href = "mailto:shizocatto@gmail.com";
+        }
+        else {
+            console.log('Edi wag.');
+          }
+    }
