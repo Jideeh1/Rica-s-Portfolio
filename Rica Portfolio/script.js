d2a017f9744eefb32569b649e2d4fdbe441269b2
Jideeh1 <keonagjd@gmail.com>
2025-01-12 16:44:17 +0800

    Add files via upload

--- Rica Portfolio/script.js
+++ Rica Portfolio/script.js
@@ -1,3 +1,4 @@
+window.onload = function() {
     setTimeout(() => {
         document.getElementById('spsc').classList.toggle('fade');
     }, 2000);
@@ -10,3 +11,20 @@
             console.log('Edi wag.');
           }
     }
+
+    document.querySelectorAll('.img').forEach(image => {
+        image.onclick = () => {
+            console.log("Image clicked:", image.getAttribute('src')); //This is for debugging only ;^;
+            document.querySelector('.modal').style.display = 'block';
+            document.querySelector('.modal img').src = image.getAttribute('src');
+        };
+    });
+    
+    document.querySelectorAll('.modal span').forEach(span => {
+        span.onclick = () => {
+            console.log("Modal closed"); //You don't know how much trouble I went through without debugging! Learned the leason hard
+            document.querySelector('section .modal').style.display = 'none';
+        };
+    });
+    
+};
